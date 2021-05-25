94f94ec070537cfc63ab5f3686c728040a914bbf
Daniel Irias Gutierrez <danielirias@gmail.com>
2021-05-25 12:21:40 -0600

    Actualización de archivo

--- 403.php
+++ 403.php
@@ -101,7 +101,7 @@
                     } );
                 },
                 "language": {
-                    "search": "Buscarx:"
+                    "search": "Buscar:"
                 }
             } );
 
